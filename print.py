63ed7fd0d780f3b9a576f4d46149fa552b12f656
Sherwyn Poon <sherwyn@0587373787.wireless.umich.net>
2017-04-13 15:36:51 -0400

    ~ classify_reviews.py

--- print.py
+++ print.py
@@ -3,7 +3,6 @@ from numpy import std, mean
 
 path = 'data/yelp_funny_b.json'
 with open(path) as json_data:
-    d = json.load(json_data)[:10]
+    d = json.load(json_data)[:100]
     for x in d:
         print x['text']
-        print '*'
